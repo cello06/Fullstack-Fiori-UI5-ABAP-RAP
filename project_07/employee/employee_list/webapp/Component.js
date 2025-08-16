1767cd40c20eef5f14c89ed5f7fe8cd0c7efb1af
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2025-08-17 00:34:54 +0300

    List Page

---
+++ project_07/employee/employee_list/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("employeelist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
